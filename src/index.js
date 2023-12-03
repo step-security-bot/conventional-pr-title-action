9bc47ed7ce1bf7798997108a26ff6510bdec0fd3
Ashish Kurmi <akurmi@stepsecurity.io>
2023-12-03 01:17:40 -0800

    adding subscription check

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const npa = require('npm-package-arg');
-const installPreset = require('./installPreset');
-const validateTitle = require('./validateTitle');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const npa = require("npm-package-arg");
+const installPreset = require("./installPreset");
+const validateTitle = require("./validateTitle");
+const axios = require("axios");
+
+async function validateSubscription() {
+  const API_URL = `https://agent.api.stepsecurity.io/v1/github/${process.env.GITHUB_REPOSITORY}/actions/subscription`;
+
+  try {
+    await axios.get(API_URL, { timeout: 3000 });
+  } catch (error) {
+    if (error.response) {
+      console.error(
+        "Subscription is not valid. Reach out to support@stepsecurity.io"
+      );
+      process.exit(1);
+    } else {
+      core.info("Timeout or API not reachable. Continuing to next step.");
+    }
+  }
+}
 
 async function run() {
+  await validateSubscription();
+
   try {
-    let contextName = core.getInput('context-name');
-    let successState = core.getInput('success-state');
-    let failureState = core.getInput('failure-state');
-    let targetUrl = core.getInput('target-url');
-    const installPresetPackage = core.getInput('preset');
+    let contextName = core.getInput("context-name");
+    let successState = core.getInput("success-state");
+    let failureState = core.getInput("failure-state");
+    let targetUrl = core.getInput("target-url");
+    const installPresetPackage = core.getInput("preset");
     const requirePresetPackage = npa(installPresetPackage).name;
 
     const client = new github.getOctokit(process.env.GITHUB_TOKEN);
@@ -33,38 +53,34 @@ async function run() {
       error = err;
     }
 
-    core.setOutput('success', (error === null).toString());
+    core.setOutput("success", (error === null).toString());
 
-    let state = 'success';
+    let state = "success";
     let description = successState;
     if (error) {
-      state = 'failure';
+      state = "failure";
       description = failureState;
     }
 
-    await client.request(
-      'POST /repos/:owner/:repo/statuses/:sha',
-      {
-        owner,
-        repo,
-        state,
-        description,
-        sha: contextPullRequest.head.sha,
-        target_url: targetUrl,
-        context: contextName,
-      },
-    );
+    await client.request("POST /repos/:owner/:repo/statuses/:sha", {
+      owner,
+      repo,
+      state,
+      description,
+      sha: contextPullRequest.head.sha,
+      target_url: targetUrl,
+      context: contextName,
+    });
 
     if (error) {
       throw error;
     } else {
       console.log(`${state}: ${description}`);
     }
-
   } catch (error) {
-    core.setOutput('error', error.message);
+    core.setOutput("error", error.message);
     core.setFailed(error.message);
   }
-};
+}
 
 run().catch(console.error);
